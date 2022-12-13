6a1c6c64a99c738caeedb214993c741c615dc60d
Jovan Ilić <jovan158@gmail.com>
2022-12-13 22:52:19 +0100

    Domaći za 13.12

--- src/main/java/p13_12_domaci/Zadatak1.java
+++ src/main/java/p13_12_domaci/Zadatak1.java
@@ -1,11 +1,6 @@
 package p13_12_domaci;
-
-import javax.sound.midi.Soundbank;
-import java.util.Scanner;
-
 public class Zadatak1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int major, minor, patch;
 
         major = 5;
