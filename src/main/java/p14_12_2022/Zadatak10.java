1dbed9842d4b0216835bd2a47137aed151f0d199
Jovan Ilić <jovan158@gmail.com>
2022-12-16 22:13:31 +0100

    Predavanje p14_12_2022

--- src/main/java/p14_12_2022/Zadatak10.java
+++ src/main/java/p14_12_2022/Zadatak10.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 /*Napisati program koji ispisuje za uneti broj a da li je deljiv sa 2 i deljiv sa 3.
 Upoznajte se sa opertorom moduo % dok ne dodjemo do tog operatora.
-
-HINT: Broj B je deljiv brojem A ukoliko je ostatak pri deljenju sa A jedan nuli.
 */
 public class Zadatak10 {
     public static void main(String[] args) {
